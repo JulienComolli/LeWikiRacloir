080dce5ce1cc11d02b8c62c411009430710ace59
JulienComolli <comolli.julien@gmail.com>
2022-06-05 16:21:00 +0200

    Première version

---
+++ parseWiki.js
@@ -0,0 +1,119 @@
+import { parse } from 'node-html-parser';
+import fetch from 'node-fetch';
+
+const CAT_GRAM = ['étymologie', 'adjectif', 'verbe', 'forme de verbe', 'nom commun', 'forme de nom commun']
+const CAT_GRAM_OL = ['adjectif', 'verbe', 'forme de verbe', 'nom commun', 'forme de nom commun']
+
+async function parseWiki(mot) {
+
+    // Récupération du contenu de la page
+    const res = await fetch('https://fr.wiktionary.org/wiki/' + mot);
+    const body = await res.text();
+
+    // Check que la page existe sinon erreur
+    if (res.status != 200) {
+        return {
+            erreur: 'Problème de récupération du mot.',
+            status: res.status
+        }
+    }
+
+    // Début du parsing
+    let response = { mot }
+
+    let nCatGram;
+    let nCatGramOl;
+    let langs = [];
+    let catGramList = [];
+    let catGramOlList = [];
+
+    const root = parse(body);
+    const parseOutput = root.querySelector('.mw-parser-output'); // Selection du contenu
+    const secTitle = parseOutput.querySelectorAll('h3'); // catégories grammaticales
+    const langTitle = root.querySelectorAll('.mw-parser-output > h2 .mw-headline .sectionlangue') // les différentes langues, français + patois
+    const ols = root.querySelectorAll('.mw-parser-output > ol');
+    const imgs = parseOutput.querySelectorAll('a.image > img');
+
+    if (imgs.length > 0) response['imgs'] = [];
+
+    imgs.forEach(img => {
+        response['imgs'].push(img.attributes.src.slice(2));
+    });
+
+    response['langs'] = langs
+    langTitle.forEach(e => {
+        langs.push(e.text)
+        response[e.text] = {}
+    }); // récupération des langues / patois (il y aura toujours Français + ...)
+
+    nCatGram = 0;
+    nCatGramOl = 0;
+    secTitle.forEach(e => {
+        const secTitle = clearString(e.querySelector('.mw-headline span').text);
+        if (CAT_GRAM.includes(secTitle)) {
+            nCatGram++;
+            catGramList.push(secTitle);
+            if (CAT_GRAM_OL.includes(secTitle)) {
+                nCatGramOl++;
+                catGramOlList.push(secTitle);
+            }
+        }
+    });
+
+    let tmpList = []
+    let langIdx = 0;
+    let olIdx = 0;
+    catGramList.forEach(cat => {
+        if (tmpList.includes(cat)) {
+            tmpList = [];
+            langIdx++;
+        }
+
+        tmpList.push(cat);
+        response[langs[langIdx]][cat] = []
+
+        if (CAT_GRAM_OL.includes(cat)) {
+            let currOl = ols[olIdx];
+            currOl = parse('<div>' + currOl.outerHTML + '</div>'); // Wrapper pour faciliter les selecteurs
+
+            let defs = currOl.querySelectorAll('ol > li');
+            let ulExs, resDef;
+            defs.forEach(def => {
+                resDef = {}
+
+                ulExs = def.querySelector('ul'); // Liste des examples d'utilisation
+                def.removeChild(ulExs);
+
+                resDef['def'] = def.innerText.replace('\n', '');
+
+                if (ulExs) {
+                    resDef['exs'] = []
+                    let exs = ulExs.querySelectorAll('li');
+                    exs.forEach(ex => {
+                        resDef['exs'].push(ex.innerText);
+                    });
+                }
+
+                response[langs[langIdx]][cat].push(resDef)
+            });
+
+
+            olIdx++;
+        }
+    });
+
+    return response;
+
+}
+
+export default parseWiki;
+
+
+
+function clearString(str) {
+    try {
+        return str.replace(/[0-9]/g, '').toLowerCase();
+    } catch {
+        return null;
+    }
+}
---
+++ server.js
@@ -0,0 +1,36 @@
+import http from 'http';
+import fs from 'fs';
+
+import parseWiki from './parseWiki.js';
+
+const PORT = 4567;
+
+
+const webPage = fs.readFileSync('./index.html');
+let apiUses = parseInt(fs.readFileSync('./apiUses.txt', 'utf-8'), 10);
+
+// Petit serveur HTTP pour répondre à la seule requête de l'API ou envoyer la page web
+const server = http.createServer(async (req, res) => {
+    if (req.method === 'GET') {
+        if (req.url.match(/\/api\/mot\/\w+/)) {
+            const mot = req.url.split('/')[3].replace('%20', '-') // On renplace les espaces par un tiret
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify(await parseWiki(mot)));
+            apiUses++;
+        } else {
+            res.writeHeader(200, { "Content-Type": "text/html" });
+            res.write(webPage);
+            res.end();
+        }
+    }
+});
+
+// Lancement du serveur
+server.listen(PORT, () => console.log(`> Serveur lancé sur le port ${PORT}`));
+
+
+// Petit bout de code pour mettre à jour le nombre de requêtes à l'API
+function updateApiUses() {
+    fs.writeFileSync('./apiUses.txt', ''+apiUses);
+}
+setInterval(updateApiUses, 60 * 60 * 1000);
