8e261ae9dd2c9993dd1c360bfff2666da00f4a07
JulienComolli <comolli.julien@gmail.com>
2022-06-16 22:45:49 +0200

    Added cors authorizations

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ const server = http.createServer(async (req, res) => {
     if (req.method === 'GET') {
         if (req.url.match(/\/api\/mot\/\w+/)) {
             const mot = req.url.split('/')[3] // On renplace les espaces par un tiret
-            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.writeHead(200, { 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*" })
             res.end(JSON.stringify(await parseWiki(mot)));
             apiUses++;
         } else {
-            res.writeHeader(200, { "Content-Type": "text/html" });
+            res.writeHeader(200, { "Content-Type": "text/html", "Access-Control-Allow-Origin": "*"  });
             res.write(webPage);
             res.end();
         }
